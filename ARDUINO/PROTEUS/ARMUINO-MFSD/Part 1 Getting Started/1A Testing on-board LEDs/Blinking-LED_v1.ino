249b716d298c774f1c9ac54b48fdd132aa5249fc
nraplata <nraplata@gmail.com>
2020-10-16 19:16:22 -0500

    Blinking LEDs
    Cadena de LEDs parpadeantes secuencialmente

---
+++ ARDUINO/PROTEUS/ARMUINO-MFSD/Part 1 Getting Started/1A Testing on-board LEDs/Blinking-LED_v1.ino
@@ -0,0 +1,28 @@
+//ARMUINO
+// Cadena de LEDs parpadeantes secuencialmente
+int led1=13;
+int led2=12;
+int led3=11;
+int led4=10;
+
+void setup() {
+  pinMode(led1,OUTPUT);
+  pinMode(led2,OUTPUT);
+  pinMode(led3,OUTPUT);
+  pinMode(led4,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(led1,HIGH);
+  delay(1000);
+  digitalWrite(led1,LOW);
+  digitalWrite(led2,HIGH);
+  delay(1000);
+  digitalWrite(led2,LOW);
+  digitalWrite(led3,HIGH);
+  delay(1000);
+  digitalWrite(led3,LOW);
+  digitalWrite(led4,HIGH);
+  delay(1000);
+  digitalWrite(led4,LOW);
+}
